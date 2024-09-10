0b9375819e46efc9fba33c852f945536da9f7377
Juliana Melo <ferreirajuliana01234@gmail.com>
2024-09-10 14:32:23 -0300

    create entities

---
+++ Composicao2/Entities/Post.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composicao2.Entities
+{
+    internal class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public List<Comment> Comments { get; set; } = new List<Comment>(); //p ser instanciada
+
+        public Post()
+        {
+        }
+        public Post(DateTime moment, string title, string content, int likes)
+        {
+            Moment = moment;
+            Title = title;
+            Content = content;
+            Likes = likes;
+        }
+    }
+}
